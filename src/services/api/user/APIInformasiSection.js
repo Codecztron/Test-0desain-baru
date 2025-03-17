af30eb0d5e9ec3e59069263010a7f1f3b5dd7c10
Ezar Iago <ezar.iago@gmail.com>
2025-03-17 23:04:24 +0700

    Referensi pertama API

--- src/services/api/user/APIInformasiSection.js
+++ src/services/api/user/APIInformasiSection.js
@@ -1,11 +1,11 @@
 // src/services/api.js
 import axios from "axios";
 
-const BASE_URL = window.location.origin;
+const BASE_URL = "http://localhost:8080/api/user/informasi";
 
 export const getInformasi = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}/data/InformasiSection.json`);
+    const response = await axios.get(`${BASE_URL}/data`);
     return response.data.informasi;
   } catch (error) {
     console.error("Error fetching informasi: ", error);
